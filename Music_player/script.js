798f32bb9bae3065b64c83493eb41398bbb58818
Yamuna H <70136290+yamunah96@users.noreply.github.com>
2026-02-19 15:17:51 +0530

    Add files via upload

---
+++ Music_player/script.js
@@ -0,0 +1,23 @@
+function playCradle() {
+  document.getElementById("audio1").play();
+}
+
+function pauseCradle() {
+  document.getElementById("audio1").pause();
+}
+
+function playIphone() {
+  document.getElementById("audio2").play();
+}
+
+function pauseIphone() {
+  document.getElementById("audio2").pause();
+}
+
+function playPikachu() {
+  document.getElementById("audio3").play();
+}
+
+function pausePikachu() {
+  document.getElementById("audio3").pause();
+}
